d1cf7bc591a717eb1bef88370789c345fade8ee3
Anders Findal Jørgensen <fj.Anders@gmail.com>
2026-04-18 18:41:42 +0200

    Made the TV yap!
    The TV now yaps. Just need to hook it into a loop and the rest of the game.

--- LD59_jam/TV-Person/TVTalk.gd
+++ LD59_jam/TV-Person/TVTalk.gd
@@ -1,36 +1,42 @@
 extends Label
 
-signal tvFaceTalk
-signal tvFaceListen
-
-var talkingFaces : Array = [":D",":)",":o","XD"]
+var talkingFaces : Array = [":)",":D",":o","XD",":|",":/",":("]
+var mouthOpen :bool
+var yapAttack :bool
 
 func _ready() -> void:
 	Global.date_is_happy.connect(on_date_is_happy)
 	Global.date_is_neutral.connect(on_date_is_neutral)
 	Global.date_is_unhappy.connect(on_date_is_unhappy)
+	Global.date_is_talking.connect(on_date_is_talking) #Hook this up somewhere!
 
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
-		faceReplace(":)")
-	else:
-		faceTalking()
+	if Input.is_action_pressed("ui_home"):
+		yapAttack = true
+	if yapAttack == true:
+		set_talking()
+
 func faceReplace(newText:String):
 	text = newText
 	
-func faceTalking():
-	for i in talkingFaces.size():
-		await get_tree().create_timer(.2).timeout 
-		text = talkingFaces[i]
+func set_talking(): #This is ment to loop
+	if mouthOpen == false:
+		await get_tree().create_timer(.1).timeout 
+		text = talkingFaces[0]
+		mouthOpen = true
+	else:
+		await get_tree().create_timer(.1).timeout 
+		text = talkingFaces[1]
+		mouthOpen = false
 
 func set_happy():
 	text = talkingFaces[0]
 	
 func set_neutral():
-	text = talkingFaces[1]
+	text = talkingFaces[4]
 	
 func set_unhappy():
-	text = talkingFaces[2]
+	text = talkingFaces[6]
 	
 func on_date_is_happy():
 	set_happy()
@@ -40,3 +46,6 @@ func on_date_is_neutral():
 	
 func on_date_is_unhappy():
 	set_unhappy()
+
+func on_date_is_talking():
+	set_talking()
